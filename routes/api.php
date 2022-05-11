9b42405c4867c6213100e351f36bbbc4e0a71d5a
Rana Hisham Foad <rana@megatrust.co>
2022-05-11 12:08:42 +0200

    editing in namespace

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Api\ApiController;
 use App\Http\Controllers\Api\AuhtController;
 use App\Http\Controllers\Api\LeaveTypeController;
 use App\Http\Controllers\Api\LeaveRequestController;
