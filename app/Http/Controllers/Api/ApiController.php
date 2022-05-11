ccdf1d22dd4c6e1c1aa31382a6dbae035f588a3b
Rana Hisham Foad <rana@megatrust.co>
2022-05-11 14:21:11 +0200

    edit in my status

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -56,6 +56,10 @@ class ApiController extends Controller
     {
         $employee = auth('api')->user();
         $check = Check::whereEmployeeId($employee->id)->latest()->first();
+        if(!$check){
+            return response()->json(['data' => null,
+            'message' => 'latest attendance status','code'=>400], 400);
+        }
         return response()->json(['data' => new CheckResource($check),
             'message' => 'latest attendance status','code'=>200], 200);
     }
